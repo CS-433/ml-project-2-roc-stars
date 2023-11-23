228a0a0448c631ed4ea96911988097393ffb9607
clarachappuis <cla28@bluewin.ch>
2023-11-23 11:06:41 +0100

    clean clean clean

--- data_cleaning.py
+++ data_cleaning.py
@@ -7,16 +7,40 @@ df_raw = pd.read_csv('data/EMemory_data.csv', sep=";", header=0)
 # drop columns with only Nan values
 df = df_raw.dropna(axis=1, how='all')
 
-# Convert to numpy array and convert <no-response> to <not shown>
-raw_data = df.to_numpy()
-raw_data[raw_data == '<not-shown>'] = '<no-response>'
-print(raw_data.shape)
+# drop all the col ending with _RT
+columns_to_drop = [col for col in df.columns if col.endswith('_RT')]
+df.drop(columns=columns_to_drop, inplace=True)
+
+# convert <no-response> to <not shown>
+df[df == '<not-shown>'] = '<no-response>'
 
 # Remove columns that contain only <no response>
-cols_to_delete = np.where((raw_data == '<no-response>').all(axis=0))[0]
-data = np.delete(raw_data, cols_to_delete, axis=1)
-print(data.shape)
+cols_no_response = df.columns[(df == '<no-response>').all(axis=0)]
+df.drop(columns=cols_no_response, inplace=True)
 
+print(df)
+
+columns_containing_cud = df.filter(like='CUD', axis=1).columns.to_numpy()
+print(columns_containing_cud[2])
+
+col_i = columns_containing_cud[1]
+print(col_i)
+print(col_i[:len(col_i)-4])
+
+columns_containing_ptsd = df.filter(like='PTSD', axis=1).columns.to_numpy()
+print(columns_containing_ptsd[2])
+
+for i in range(len(columns_containing_cud)):
+    col_i = columns_containing_cud[i]
+    for j in range(len(columns_containing_ptsd)):
+        col_j = columns_containing_ptsd[j]
+        if (col_i[:len(col_i) - 4] == col_j[:len(col_j) - 4]):
+            print(col_j)
+            print("Colums ", i, "and ", j, "are the same experiment")
+
+df.to_csv("new_data.csv", sep = ',')
+
+data = df.to_numpy()
 # Separate PTSD from CUD patients
 PTSD = (data[:, 6] == 'Intrusionsfragebogen (T)')
 CUD = (data[:, 6] == 'Intrusionsfragebogen (K)')
