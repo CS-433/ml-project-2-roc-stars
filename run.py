4a7e6a6f08fce37b68b2f3b5ce2ff1ffb17b0053
clarachappuis <cla28@bluewin.ch>
2023-11-27 17:08:59 +0100

    modified clean.py --> run.py

--- run.py
+++ run.py
@@ -2,5 +2,5 @@ import pandas as pd
 import numpy as np
 
 # Load dataset
-df = pd.read_csv('data/cleaned_data.csv', sep=",", header=0)
+df = pd.read_csv('data/final_data.csv', sep=";", header=0)
 print(df)
