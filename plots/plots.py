6888c674ab5c19326462ba2e535ecc820a319716
clarachappuis <cla28@bluewin.ch>
2023-12-04 14:24:01 +0100

    visualization of columns distributions in plots.py

--- plots/plots.py
+++ plots/plots.py
@@ -4,3 +4,24 @@ import matplotlib.pyplot as plt
 
 df = pd.read_csv('data/EMemory_data.csv', sep=";", header=0)
 
+# Visualization of the columns distributions
+col1_25 = df.columns[:25].to_numpy()
+col26_50 = df.columns[26:50].to_numpy()
+col51_75 = df.columns[51:75].to_numpy()
+col76_86 = df.columns[76:].to_numpy()
+cols = [col1_25, col26_50, col51_75, col76_86]
+
+for col_set in cols:
+    data = []
+    for i in col_set[1:]:
+        data.append(df[i])
+
+    fig = plt.figure(figsize=(30, 14))
+    name  = col_set[1:]
+
+    for i in range(len(data)):   
+        plt.subplot(5, 5,1+i)
+        plt.hist(data[i], bins=150)
+        plt.title(name[i])
+    plt.tight_layout(pad=3.0)
+    plt.show()
--- run.py
+++ run.py
@@ -1,5 +1,6 @@
 import pandas as pd
 import numpy as np
+import matplotlib.pyplot as plt
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
 from sklearn.metrics import f1_score, accuracy_score
