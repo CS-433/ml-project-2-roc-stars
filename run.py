cddb2e3cf47458628a4e886ad21cb09b384190f2
clarachappuis <cla28@bluewin.ch>
2023-12-04 13:43:24 +0100

    add L2 logistic regression

--- run.py
+++ run.py
@@ -2,7 +2,7 @@ import pandas as pd
 import numpy as np
 from sklearn.linear_model import LogisticRegression
 from sklearn.model_selection import train_test_split
-from sklearn.metrics import f1_score
+from sklearn.metrics import f1_score, accuracy_score
 from sklearn.metrics import confusion_matrix
 
 # Load dataset
@@ -13,16 +13,23 @@ X = df.drop(columns=['SURVEY_NAME'])
 y = df['SURVEY_NAME']
 
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
-logreg = LogisticRegression()
+
+# Logistic regression without penalty
+logreg = LogisticRegression(penalty = None)
 logreg.fit(X_train, y_train)
-
 y_pred = logreg.predict(X_test)
-print('Accuracy: {:.2f}'.format(logreg.score(X_test, y_test)))
-
+acc = accuracy_score(y_test, y_pred)
 f1 = f1_score(y_test, y_pred)
-
+print('Accuracy: ', acc)
 print("F1 Score:", f1)
 
+# Logistic regression with L2 penalty
+logreg_l2 = LogisticRegression(penalty = 'l2', max_iter = 500, fit_intercept = True, solver = 'lbfgs')
+logreg_l2.fit(X_train, y_train)
+y_pred_l2 = logreg_l2.predict(X_test)
+acc_l2 = accuracy_score(y_test, y_pred_l2)
+f1_l2 = f1_score(y_test, y_pred_l2)
+print('Accuracy: ', acc_l2)
+print("F1 Score:", f1_l2)
 
-confusion_matrix = confusion_matrix(y_test, y_pred)
-print(confusion_matrix)
+
