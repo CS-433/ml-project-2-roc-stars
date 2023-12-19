621e2c924ef7edfe9fe7017e525a25ec376e7d78
ch-epfl-326866 <ch-epfl-326866@noto.epfl.ch>
2023-12-19 11:07:13 +0100

    Improved ethics figures

--- ethics.py
+++ ethics.py
@@ -5,15 +5,21 @@ import matplotlib.pyplot as plt
 # Directory where the images will be saved
 path = "plots/ethics/"
 
+# Set figures dimensions
+im_size = (8,6)
+
+# Set figures fontisze
+font_size = 12
 
 # Load dataset
 df = pd.read_csv('data/Data_Patients.csv', sep=";", header=0)
 
 # Plot histogram of participants Age
+plt.figure(figsize=im_size) 
 plt.hist(df['age'], bins=10, color='skyblue', edgecolor='black')
-plt.xlabel('Age')
-plt.ylabel('Count')
-plt.title('Age distribution across participants')
+plt.xlabel('Age', fontsize = font_size)
+plt.ylabel('Count', fontsize = font_size)
+#plt.title('Age distribution across participants')
 plt.savefig(path + "age.png")
 plt.show()
 
@@ -23,8 +29,7 @@ df['sex'] = df['sex'].map({0: 'Female', 1: 'Male'})
 #ATTENTION IL FAUT DEMANDER SI O = HOMME OUR FEMME
 plt.figure(figsize=(8, 8))
 age_counts = df['sex'].value_counts()
-plt.pie(age_counts, labels=age_counts.index, autopct='%1.1f%%', startangle=90, colors=plt.cm.Paired.colors)
-plt.title('Participants Gender')
+plt.pie(age_counts, labels=age_counts.index, autopct='%1.1f%%', startangle=90, colors=plt.cm.Paired.colors, textprops={'fontsize': 18})
 plt.savefig(path + "gender.png")
 plt.show()
 
@@ -33,10 +38,10 @@ plt.show()
 # Replace commas with decimal points and convert to numeric
 df['years_of_education'] = pd.to_numeric(df['years_of_education'].str.replace(',', '.'), errors='coerce')
 # Plot Histogram
+plt.figure(figsize=(8, 8))
 plt.hist(df['years_of_education'] , bins=5, color='skyblue', edgecolor='black')
-plt.xlabel('Years of eductation')
-plt.ylabel('Count')
-plt.title('Participants years of education')
+plt.xlabel('Years of eductation', fontsize = font_size)
+plt.ylabel('Count', fontsize = font_size)
 plt.savefig(path + "education.png")
 plt.show()
 
@@ -54,11 +59,10 @@ counts =category_counts.values
 countries = category_counts.index.tolist()
 # Plotting
 
-plt.figure(figsize=(12, 8))
+plt.figure(figsize=(8, 8))
 plt.barh(countries, counts, color='skyblue')
-plt.xlabel('Counts')
-plt.ylabel('Countries')
-plt.title('Number of Occurrences of patients origins by Country')
+plt.xlabel('Counts', fontsize = font_size)
+plt.ylabel('Countries', fontsize = font_size)
 #plt.yticks(countries[::2]) Optional makes it clear but not all countries are displayed
 plt.savefig(path + "origins.png")
 plt.show()
