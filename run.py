92015c9c9219f8cac3a887bea5d5f387fef4314d
renukasinghvirk <renuka.singhvirk@epfl.ch>
2023-12-20 15:38:04 +0100

    modified run

--- run.py
+++ run.py
@@ -2,8 +2,7 @@
 from helper import *
 import pandas as pd
 from sklearn.model_selection import train_test_split
-from sklearn.neural_network import MLPClassifier
-from sklearn.metrics import f1_score, accuracy_score
+from sklearn.linear_model import LogisticRegression
 
 # < -----------------------------------Load Data----------------------------------------- >
 # Load dataset
@@ -12,10 +11,7 @@ X = df.drop(columns=['SURVEY_NAME'])
 y = df['SURVEY_NAME']
 X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=0)
 
-# < --------------------Display best accuracy and F1 scores------------------------------ >
-mlp = (MLPClassifier(hidden_layer_sizes=(50, 50), max_iter=1000, random_state=42, beta_2=0.7, beta_1=0.9, solver='adam', activation='relu', batch_size=64))
-mlp.fit(X_train, y_train)
-y_pred_mlp = mlp.predict(X_test)
-acc_mlp = accuracy_score(y_pred_mlp, y_test)
-f1_mlp = f1_score(y_pred_mlp, y_test)
-print("MLP\n","Accuracy: ", acc_mlp, "\n", "F1 score :", f1_mlp) 
+# < -------------------------------Logistic Regression------------------------------------ >
+logistic_regression = LogisticRegression(random_state=42, penalty='l2', C=1.0, max_iter=2000)
+logistic_regression.fit(X_train, y_train)
+model_performance(logistic_regression, X_test, y_test, CV=False)
